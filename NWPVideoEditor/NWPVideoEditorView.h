7cc4ef9a0faad54d0b9f3e5af4f9fcf323155975
MM <matej261matej@gmail.com>
2025-09-06 21:24:57 +0200

    Implemented clip splitting on right click

--- NWPVideoEditor/NWPVideoEditorView.h
+++ NWPVideoEditor/NWPVideoEditorView.h
@@ -8,17 +8,16 @@ struct ClipItem {
     int     iImage = -1;
 };
 
-// New structure for timeline clips
 struct TimelineClip {
     CString path;
-    double startTimeOnTimeline = 0.0;  // Position on the timeline
-    double clipStartSec = 0.0;         // Start position within the source clip
-    double clipLengthSec = 10.0;       // Length of the clip segment
-    double originalDuration = 30.0;     // Original duration of the source clip
+    double startTimeOnTimeline = 0.0;
+    double clipStartSec = 0.0;
+    double clipLengthSec = 10.0;
+    double originalDuration = 30.0;
     int iImage = -1;
 };
 
-class CNWPVideoEditorDoc; // Forward declaration
+class CNWPVideoEditorDoc;
 
 class NWPVideoEditorView : public CView
 {
@@ -29,26 +28,22 @@ protected:
 public:
     CNWPVideoEditorDoc* GetDocument() const;
 
-    // Media grid
     CListCtrl   m_list;
     CImageList  m_imgLarge;
-    std::vector<ClipItem> m_importedClips; // Renamed from m_timeline
+    std::vector<ClipItem> m_importedClips;
 
-    // Preview window
-    CRect       m_rcPreview;            // Preview rectangle
-    HBITMAP     m_hPreviewBitmap;       // Current preview frame
-    CString     m_currentPreviewPath;   // Currently previewed file
-    double      m_previewTimePosition;  // Current time position in preview
+    CRect       m_rcPreview;
+    HBITMAP     m_hPreviewBitmap;
+    CString     m_currentPreviewPath;
+    double      m_previewTimePosition;
 
-    // Timeline model - changed to support multiple clips
     std::vector<TimelineClip> m_timelineClips;
-    int m_activeTimelineClipIndex = -1; // Index of currently selected timeline clip
+    int m_activeTimelineClipIndex = -1;
     CRect   m_rcTimeline;
 
     struct OverlayItem { CString text; double startSec; double durSec; };
     std::vector<OverlayItem> m_overlays;
 
-    // Dragging states
     enum DragState { DRAG_NONE, DRAG_LEFT_HANDLE, DRAG_RIGHT_HANDLE, DRAG_CLIP_MOVE };
     DragState m_dragState = DRAG_NONE;
     CPoint m_dragStart{};
@@ -56,15 +51,17 @@ public:
     double m_dragStartClipLength = 0.0;
     double m_dragStartTimelinePos = 0.0;
 
-    // Drag-drop members for clip list
     int m_nDragIndex = -1;
     BOOL m_bDragging = FALSE;
     CImageList* m_pDragImage = nullptr;
 
-    // Timeline settings
-    double m_timelineDurationSec = 60.0; // Total timeline duration
+    double m_timelineDurationSec = 60.0;
     double m_timelineScrollOffset = 0.0;
-    int m_contextMenuClipIndex = -1;  // tracks which clip was right-clicked
+    int m_contextMenuClipIndex = -1;
+
+    double m_hoverTimePosition = -1.0;
+    int m_hoverClipIndex = -1;
+    bool m_showSelectionBar = false;
 
 public:
     virtual void OnDraw(CDC* pDC);
@@ -87,16 +84,17 @@ protected:
     afx_msg void OnSize(UINT nType, int cx, int cy);
     afx_msg void OnFileImport();
     afx_msg void OnFileExport();
+    afx_msg void OnFileSave();
     afx_msg void OnLButtonDown(UINT nFlags, CPoint pt);
     afx_msg void OnMouseMove(UINT nFlags, CPoint pt);
     afx_msg void OnLButtonUp(UINT nFlags, CPoint pt);
     afx_msg void OnRButtonDown(UINT nFlags, CPoint pt);
-    // List control event handlers
     afx_msg void OnListDblClk(NMHDR* pNMHDR, LRESULT* pResult);
     afx_msg void OnListItemActivate(NMHDR* pNMHDR, LRESULT* pResult);
     afx_msg void OnListBeginDrag(NMHDR* pNMHDR, LRESULT* pResult);
     afx_msg void OnEditAddText();
     afx_msg void OnTimelineRemoveClip();
+    afx_msg void OnTimelineSplitClip();
     DECLARE_MESSAGE_MAP()
 
 private:
@@ -117,12 +115,11 @@ private:
     int SecondsToTimelineX(double seconds) const;
     double GetVideoDuration(const CString& filePath);
     void RepositionClipsAfterRemoval();
-    // Helper functions for drag-drop
     BOOL IsOverTimeline(CPoint screenPt);
     void AddClipToTimeline(const CString& clipPath);
 };
 
-#ifndef _DEBUG  // debug version in NWPVideoEditorView.cpp
+#ifndef _DEBUG
 inline CNWPVideoEditorDoc* NWPVideoEditorView::GetDocument() const
 {
     return reinterpret_cast<CNWPVideoEditorDoc*>(m_pDocument);
